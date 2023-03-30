16250d0f8d5d196b92eccd3b0032445876e82e37
Lardzzz <aku.ikan04@gmail.com>
2023-03-30 10:39:26 +0700

    membuat array

--- MergeSortnQuickSort.cpp
+++ MergeSortnQuickSort.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-//membuat arrat
+//membuat array
 int arr[20];
 int cmp_count = 0; //number of comparison
 int mov_count = 0; //number of data movements
 int n;
+
+
+void input() {
+
+	while (true)
+	{
+		cout << "Masukkan Panjang Element Array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum Panjang Array Adalah 20" << endl;
+	}
+}
+
+int maain() {
+
+}
