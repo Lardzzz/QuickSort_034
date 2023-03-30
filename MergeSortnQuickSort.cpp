98d4b57b03e74f5f4fb5fc20670aad80abbbc1d3
Lardzzz <aku.ikan04@gmail.com>
2023-03-30 11:33:17 +0700

    prosedur quicksort

--- MergeSortnQuickSort.cpp
+++ MergeSortnQuickSort.cpp
@@ -39,4 +39,51 @@ void swap(int x, int y) {
 	arr[y] = temp;
 }
 
+//prosedur quicksort
+void q_sort(int low, int high) {
+	int pivot, i, j;
+	if (low > high)										//langkah 1
+		return;
+
+	//pisah list menjadi dua bagian
+	//1 bagian mengandung elemen yang lebih kecil atau sama dengan pivot
+	//bagian lain mengandung elemen yang lebih besar dari pivot
+
+	pivot = arr[low];									//langkah 2
+
+	i = low + 1;										//langkah 3
+	j = high;											//langkah 4
+
+	while (i <= j) {									//langkah 10
+		//mencari elemen yang lebih besar dari pivot
+		while ((arr[i] <= pivot) && (i <= high)) {		//langkah 5
+			i++;
+			cmp_count++;								//langkah 6
+		}
+		//mencari elemen yang lebih kecil dari pivot
+		while ((arr[j] > pivot) && (j >= low)) {		//langkah 7
+			j--;
+			cmp_count++;								//langkah 8
+		}
+		cmp_count++;
+
+		if (i < j) { //jika elemen lebih besar di kiri elemen		//langkah 9
+			//ubah elemen di indeks 1 dengan elemen indeks j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j menganduk indeks dari elemen terakhir sorted list
+	if (low < j) {													//langkah 11
+		//ubah pivot ke posisi yang benar dari list
+		swap(low, j);
+		mov_count++;
+		}
+	//urutkan list di kiri menggunakan quick sort
+	q_sort(low, j - 1);												//langkah 12
+	
+}
+
+
+
 
